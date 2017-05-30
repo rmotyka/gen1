46d1c4d5b870fda2783ba6a2fffa1f9cb1699ea5
Roman Motyka <r.motyka@cctechonology.pl>
2017-05-30 15:33:48 +0200

    Save graphics with cities

--- graphics/graphics.go
+++ graphics/graphics.go
@@ -7,16 +7,21 @@ import (
 )
 
 func SaveCityImage(maxCoordinate int, cityList []structs.City) {
-	width := 500
-	height := 500
+	width := maxCoordinate
+	height := maxCoordinate
 
 	f, _ := os.Create("outsvg.svg")
 	defer f.Close()
 
 	canvas := svg.New(f)
 	canvas.Start(width, height)
-	canvas.Circle(width/2, height/2, 100)
-	canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
+
+	for _, city := range cityList {
+		canvas.Circle(city.X, city.Y, 2)
+	}
+
+
+	//canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
 	canvas.End()
 
 
